fb47a438681c4a5152e16064df51504dcf3325ef
abhishek17551 <ababhishek113@gmail.com>
2024-06-15 16:23:16 +0530

    defined types for Notes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,29 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { Container } from 'react-bootstrap'
 import { Navigate, Route, Routes } from 'react-router-dom'
+import NewNote from './components/NewNote'
 
+export type NoteData = {
+  title : string,
+  body : string,
+  tags : Tag[]
+}
+
+export type Note ={
+  id : string
+} & NoteData
+
+export type Tag = {
+  id : string,
+  label : string
+}
 
 function App() {
   return (
     <Container className='my-4'>
       <Routes>
         <Route path='/' element={<h1>Home</h1>}/>
-        <Route path='/new' element={<h1>New</h1>}/>
+        <Route path='/new' element={<NewNote/>}/>
         <Route path='/:id'>
           <Route index element={<h1>Show</h1>}/>
           <Route path='edit' element={<h1>Edit</h1>}/>
